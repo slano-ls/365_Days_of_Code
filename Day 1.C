e15f2b377ce89afa4bf3b5a8ed329296c5e83df9
slano-ls <laws0817@gmail.com>
2023-01-01 21:33:47 -0500

    Stuck, will continue tomorrow

--- Day 1.C
+++ Day 1.C
@@ -1,6 +1,67 @@
+//      $$$$$$\   $$$$$$\  $$$$$$$\        $$$$$$$\                                                 $$$$$$\         $$$$$$\                  $$\
+//     $$ ___$$\ $$  __$$\ $$  ____|       $$  __$$\                                               $$  __$$\       $$  __$$\                 $$ |
+//     \_/   $$ |$$ /  \__|$$ |            $$ |  $$ | $$$$$$\  $$\   $$\  $$$$$$$\        $$$$$$\  $$ /  \__|      $$ /  \__| $$$$$$\   $$$$$$$ | $$$$$$\
+//        $$$$$ / $$$$$$$\  $$$$$$$\        $$ |  $$ | \____$$\ $$ |  $$ |$$  _____|      $$  __$$\ $$$$\           $$ |      $$  __$$\ $$  __$$ |$$  __$$\
+//       \___$$\ $$  __$$\ \_____$$\       $$ |  $$ | $$$$$$$ |$$ |  $$ |\$$$$$$\        $$ /  $$ |$$  _|          $$ |      $$ /  $$ |$$ /  $$ |$$$$$$$$ |
+//     $$\   $$ |$$ /  $$ |$$\   $$ |      $$ |  $$ |$$  __$$ |$$ |  $$ | \____$$\       $$ |  $$ |$$ |            $$ |  $$\ $$ |  $$ |$$ |  $$ |$$   ____|
+//     \$$$$$$  | $$$$$$  |\$$$$$$  |      $$$$$$$  |\$$$$$$$ |\$$$$$$$ |$$$$$$$  |      \$$$$$$  |$$ |            \$$$$$$  |\$$$$$$  |\$$$$$$$ |\$$$$$$$\
+//      \______/  \______/  \______/       \_______/  \_______| \____$$ |\_______/        \______/ \__|             \______/  \______/  \_______| \_______|
+//                                                             $$\   $$ |
+//                                                             \$$$$$$  |
+//                                                              \______/
+// Author : Saihaj Law
+// Date : JAN 1st, 2023
+// Project :
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define BUFFER_SIZE 2048
+#define SEC_VAL 365
+#define ENCRYPTION_EQ(av) (SEC_VAL * 4 + 8 - 13 * 2 - 100)
+void encryption_func();
 int main(int argc, char *argv[]) {
+    char buffer[BUFFER_SIZE];
+    FILE *file = NULL;
+
+    // Open file
+    file = fopen("text.txt", "w");
+
+    if (file == NULL){
+        fprintf(stderr, "Error Opening File\n");
+        return 1;
+    }
+    printf("Hello and welcome to the STE; or the Simple Text Editor!\n");
+    printf("This is a simple text editor which does not have any markup syntax, and will be saved as a simple .txt file!\n");
+    printf("Feel free to Enter any message you want, and when you are done type in <Ctrl-D> and it will automatically save and quit!\n");
+    int ch;
+    while ((ch = getchar ()) != EOF){
+            if (tolower(ch) == 's'){
+                break;
+            }
+        fputs(buffer, file);
+    }
+
+    fclose(file);
+
+    char c;
+    int ascii_value, encrypt_value;
+    int list[BUFFER_SIZE];
+    int i=0;
+
+    file = fopen("text.txt", "r");
+    if (file == NULL){
+        perror("Couldn't open file");
+        exit(EXIT_FAILURE);
+    }
+    while((c = fgetc(file)) != EOF){
+        ascii_value = (int)c;
+        encrypt_value = ENCRYPTION_EQ(ascii_value);
+        list[i] = encrypt_value;
+        printf("The value of %c is %d \n", c, list[i]);
+        i++;
+    }
+    fclose(file);
 
     return 0;
 }
