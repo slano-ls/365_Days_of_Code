96e66dc055a0d23446dbd52d57552d4c2c157621
Saihaj Law <laws0817@gmail.com>
2023-03-09 09:09:22 -0500

    Additions

--- breakout.py
+++ breakout.py
@@ -1,8 +1,9 @@
 
 import pygame
 from pygame.locals import *
-
+import random
 pygame.init()
+
 
 screen_width = 600
 screen_height = 600
@@ -15,34 +16,73 @@ font = pygame.font.SysFont('Constantia', 30)
 
 # define colours
 bg = (0, 0, 0)
-# block colours
-block_red = (242, 85, 96)
-block_green = (86, 174, 87)
-block_blue = (69, 177, 232)
-# paddle colours
-paddle_col = (142, 135, 123)
-paddle_outline = (100, 100, 100)
+# # block colours
+# block_darkcyan = (242, 85, 96)
+# block_keppel = (86, 174, 87)
+# block_tiffany = (69, 177, 232)
+# # paddle colours
+# block_mayablue = (142, 135, 123)
+# block_indigo= (100, 100, 100)
 # text colour
+block_darkcyan = (33, 137, 126)
+block_keppel = (59, 169, 156)
+block_tiffany = (105, 209, 197)
+block_mayablue = (126, 188, 230)
+block_indigo = (137, 128, 245)
 text_col = (255, 255, 255)
 
-# themes 
-# colourful 
-#block_brightred = (255, 55, 48)
-#bl
+# theme 1 bright 
+# block_brightred = (255, 55, 48)
+# block_brightyellow = (245, 247, 87)
+# block_brightgreen = (87, 247, 87)
+# block_brightblue = (87, 167, 247)
+
+# theme 2 sea blues 
+ # block_darkcyan = (30, 37, 250)
+ # block_keppel = (44, 136, 242)
+ # block_tiffany = (194, 232, 255)
+ # block_mayablue = (126, 188, 230)
+ # block_indigo = (137, 128, 245)
+
+# theme 3 forest greens
+# block_darkgreen = (11, 61, 1)
+# block_pigment = (67, 250, 30)
+# block_highlighter = (180, 250, 30)
+# block_dartmouth = (4, 181, 107)
+# block_grass = (5, 59, 6)
+
+# theme 4 pastel
+# block_pink = (225, 193, 207)
+# block_yellow = (232, 255, 183)
+# block_mauve = (226, 160, 255)
+# block_lightcyan = (196, 245, 252)
+# block_aquamarine = (183, 255, 216)
 
 # define game variables
 cols = 6
 rows = 6
 clock = pygame.time.Clock()
-fps = 60
+fps = 80
 live_ball = False
 game_over = 0
-
+global score
+score = 0
+lives = 3
 
 # function for outputting text onto the screen
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+  
+# display score
+def display_score(score):
+    score_surf = font.render(f"Score: {score}", True, text_col)
+    screen.blit(score_surf, (10, 550))
+  
+# display lives
+def display_lives(lives):
+   display_lives = font.render(f"Lives: {lives}", True, text_col)
+   screen.blit(display_lives, (440, 550))
 
 
 # brick wall class
@@ -52,6 +92,7 @@ class wall():
         self.height = 50
 
     def create_wall(self):
+        block_number = 0
         self.blocks = []
         # define an empty list for an individual block
         block_individual = []
@@ -75,6 +116,7 @@ class wall():
                 block_individual = [rect, strength]
                 # append that individual block to the block row
                 block_row.append(block_individual)
+                block_number += 1
             # append the row to the full list of blocks
             self.blocks.append(block_row)
 
@@ -83,15 +125,14 @@ class wall():
             for block in row:
                 # assign a colour based on block strength
                 if block[1] == 3:
-                    block_col = block_blue
+                    block_col = block_tiffany
                 elif block[1] == 2:
-                    block_col = block_green
+                    block_col = block_keppel
                 elif block[1] == 1:
-                    block_col = block_red
+                    block_col = block_darkcyan
                 pygame.draw.rect(screen, block_col, block[0])
                 pygame.draw.rect(screen, bg, (block[0]), 2)
-
-
+        
 # paddle class
 class paddle():
     def __init__(self):
@@ -109,8 +150,8 @@ class paddle():
             self.direction = 1
 
     def draw(self):
-        pygame.draw.rect(screen, paddle_col, self.rect)
-        pygame.draw.rect(screen, paddle_outline, self.rect, 3)
+        pygame.draw.rect(screen, block_mayablue, self.rect)
+        pygame.draw.rect(screen, block_indigo, self.rect, 3)
 
     def reset(self):
         # define paddle variables
@@ -118,8 +159,8 @@ class paddle():
         self.width = int(screen_width / cols)
         self.x = int((screen_width / 2) - (self.width / 2))
         self.y = screen_height - (self.height * 2)
-        self.speed = 10
-        self.rect = Rect(self.x, self.y, self.width, self.height)
+        self.speed = 9
+        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.direction = 0
 
 
@@ -128,8 +169,10 @@ class game_ball():
     def __init__(self, x, y):
         self.reset(x, y)
 
-    def move(self):
 
+    def move(self):
+        global score
+        global lives
         # collision threshold
         collision_thresh = 5
 
@@ -144,15 +187,23 @@ class game_ball():
                     # check if collision was from above
                     if abs(self.rect.bottom - item[0].top) < collision_thresh and self.speed_y > 0:
                         self.speed_y *= -1
+                        score += 1 
+                        display_score(score)
                     # check if collision was from below
                     if abs(self.rect.top - item[0].bottom) < collision_thresh and self.speed_y < 0:
                         self.speed_y *= -1
+                        score += 1 
+                        display_score(score)
                     # check if collision was from left
                     if abs(self.rect.right - item[0].left) < collision_thresh and self.speed_x > 0:
                         self.speed_x *= -1
+                        score += 1 
+                        display_score(score)
                     # check if collision was from right
                     if abs(self.rect.left - item[0].right) < collision_thresh and self.speed_x < 0:
                         self.speed_x *= -1
+                        score += 1 
+                        display_score(score)
                     # reduce the block's strength by doing damage to it
                     if wall.blocks[row_count][item_count][1] > 1:
                         wall.blocks[row_count][item_count][1] -= 1
@@ -178,7 +229,26 @@ class game_ball():
         if self.rect.top < 0:
             self.speed_y *= -1
         if self.rect.bottom > screen_height:
-            self.game_over = -1
+          
+        # lives
+          if self.rect.bottom > screen_height:
+
+            # subtract a life
+            lives -= 1 
+            ball.reset(screen_width // 2, screen_height //2)
+            paddle.reset(self)
+            display_lives = font.render("Lives:" + str(lives), True, text_col)
+            screen.blit(display_lives, (10, 10))
+            pygame.display.update()
+            if lives == 0:
+              pygame.quit()
+            # else:
+              # # reset the paddle and display remaining lives
+              # ball.reset(screen_width // 2, screen_height //2)
+              # paddle.reset(self)
+              # display_lives = font.render("Lives:" + str(lives), True, text_col)
+              # screen.blit(display_lives, (10, 10))
+              # pygame.display.update()
 
         # look for collission with paddle
         if self.rect.colliderect(player_paddle):
@@ -199,16 +269,16 @@ class game_ball():
         return self.game_over
 
     def draw(self):
-        pygame.draw.circle(screen, paddle_col, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
+        pygame.draw.circle(screen, block_mayablue, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
                            self.ball_rad)
-        pygame.draw.circle(screen, paddle_outline, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
+        pygame.draw.circle(screen, block_indigo, (self.rect.x + self.ball_rad, self.rect.y + self.ball_rad),
                            self.ball_rad, 3)
 
     def reset(self, x, y):
         self.ball_rad = 10
         self.x = x - self.ball_rad
         self.y = y
-        self.rect = Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
+        self.rect = pygame.Rect(self.x, self.y, self.ball_rad * 2, self.ball_rad * 2)
         self.speed_x = 4
         self.speed_y = -4
         self.speed_max = 5
@@ -265,7 +335,12 @@ while run:
             player_paddle.reset()
             wall.create_wall()
 
+    wall.draw_wall()
+    display_lives(lives)
+    display_score(score)
     pygame.display.update()
 
+
+    
 pygame.quit()
 
